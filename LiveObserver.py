2669bc04bab9b57b00ddbfc2ce32698a540b16f8
Spotlight0xff <spotlight0xff@gmail.com>
2016-09-12 00:28:04 +0200

    insert match record into overview on new match

--- LiveObserver.py
+++ LiveObserver.py
@@ -33,13 +33,58 @@ class LiveObserver:
                 return m
         return None
 
-    def new(self, uniq_match, matches):
+    def new(self, uniq_match, matches, tournaments):
         """gets called when a match goes live"""
         matchId = uniq_match.getMatch()
         tId = uniq_match.getTournament()
         logger.info('Match {} just went live.'.format(matchId))
         unique_name = uniq_match.getName()
         logger.debug('Unique name: ' + unique_name)
+
+        match = self.crossRefMatch(uniq_match, matches)
+
+
+        result = self.db_conn.selectRow('matches', {
+                                            'year': uniq_match.getYear(),
+                                            'match_id': matchId,
+                                            'tournament_id': tId})
+        if not result: # need to create new record
+            logger.info('Create new match record for '+unique_name)
+            t_name = ''
+            t_cat = ''
+            for t in tournaments:
+                if t.get('year') == uniq_match.getYear() and t.get('id') == tId:
+                    t_name = t.get('name')
+                    t_cat = t.get('group')
+                    break
+
+            res_ins = self.db_conn.insertRow('matches', (
+                0,
+                uniq_match.getYear(),
+                tId,
+                matchId,
+                unique_name,
+                t_name, # tournament name
+                t_cat, # tournament category
+                match.get('state'), # status
+                '0', # is doubles
+                match.get('isQuals'),
+                match.get('numSets'),
+                '{} {}'.format(match.get('nAF'), match.get('nAL')),
+                '{} {}'.format(match.get('nBF'), match.get('nBL')),
+                match.get('rnd'), # round
+                match.get('winner'), # not sure if this is correct...
+                ''.join([match.get(s) for s in ['s{}{}'.format(num,side) for side in ['A','B'] for num in range(1,6)]]),
+                '', # first server
+                match.get('ts'), # start time
+                match.get('mt'), # match time
+                '', # retirement
+                ))
+            if not res_ins:
+                logger.error('Could not create match record for '+unique_name)
+        else: # already listed (maybe check if set to live..?)
+            True
+
 
         self.map_writer[unique_name] = [self.csv_write, self.update_db]
         self.counter[unique_name] = 0
