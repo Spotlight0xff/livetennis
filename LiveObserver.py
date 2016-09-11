2cba6b6ca939fa71914d6d5e9f46dbc3583a2fc7
Spotlight0xff <spotlight0xff@gmail.com>
2016-09-11 23:00:04 +0200

    implemented database updates

--- LiveObserver.py
+++ LiveObserver.py
@@ -1,10 +1,12 @@
 import logging
 import csv
 import os.path
+import pymysql
 
 import Utils
 import Crypt
 import FeedUpdater
+import DatabaseConnection
 
 logger = logging.getLogger('root')
 
@@ -14,6 +16,12 @@ class LiveObserver:
     """
     map_writer = {}
     counter = {}
+    csvdir = 'data'
+
+    def __init__(self, csvdir, db_host, db_port, db_user, db_password, db_name):
+        self.csvdir = csvdir
+        self.db_conn = DatabaseConnection.DatabaseConnection(db_host, db_port, db_user, db_password, db_name)
+
 
     def crossRefMatch(self, uniq_match, matches):
         """Match one record in the provided arguments
@@ -39,7 +47,7 @@ class LiveObserver:
     def csv_write(self, uniq_match, match):
         """Write live match details into csv-file"""
         unique_name = uniq_match.getName()
-        csv_file = os.path.join('data',unique_name+'.csv')
+        csv_file = os.path.join(self.csvdir,unique_name+'.csv')
         new = False # need to add header @ top
         if not os.path.isfile(csv_file):
             new = True
@@ -53,7 +61,32 @@ class LiveObserver:
 
 
     def update_db(self, uniq_match, match):
+        """Update live match details in database"""
+        if not self.db_conn.success:
+            logger.error('fail')
+            return # no db support (failed to connect probably)
+
         unique_name = uniq_match.getName()
+        logger.debug('Write updates to database for \'{}\''.format(unique_name))
+        if self.db_conn.existsTable(unique_name):
+            result = self.db_conn.insertRow(unique_name, FeedUpdater.getMatchRow(match, 0))
+            if not result or result < 1:
+                logger.error('Failed to insert data into table {}'.format(unique_name))
+                return
+            logger.debug('Inserted data into ' + unique_name)
+        else:
+            result = self.db_conn.createTable(unique_name, FeedUpdater.getMatchHeader())
+            self.db_conn.cache_tableexists[unique_name] = True # add table name to cache
+            if not result:
+                logger.error('Could not create new table for \'{}\''.format(unique_name))
+                return # handle this case!
+            else:
+                result_ins = self.db_conn.insertRow(unique_name, FeedUpdater.getMatchRow(match, 0))
+                if not result_ins or result_ins < 1:
+                    logger.error('Failed to insert data into new table {}'.format(unique_name))
+                    return
+            logger.debug('Inserted data into new ' + unique_name)
+
 
 
 
