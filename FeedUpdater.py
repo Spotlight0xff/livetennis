435588bf9b47c74db1d34d5a6ae51aa99efe03c4
André Merboldt <andre.merboldt@rwth-aachen.de>
2016-09-13 19:38:00 +0200

    return match record (for overview)

--- FeedUpdater.py
+++ FeedUpdater.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from collections import OrderedDict
 import logging
 logger = logging.getLogger('root')
 
@@ -16,6 +17,39 @@ def getMatchSet(match):
     elif match.get('s1A') != '' or match.get('s1B') != '':
         set = 1
     return set
+
+def getMatchRecord(uniq_match, match, tournaments, initial):
+    unique_name = uniq_match.getName()
+    t_name = ''
+    t_cat = ''
+    for t in tournaments:
+        if t.get('year') == uniq_match.getYear() and t.get('id') == uniq_match.getTournament():
+            t_name = t.get('name')
+            t_cat = t.get('group')
+            break
+    record = OrderedDict()
+    if initial:
+        record['id'] = 0 # will get autoincremented anyway
+    record['year'] = uniq_match.getYear()
+    record['tournament_id'] = uniq_match.getTournament()
+    record['match_id'] = uniq_match.getMatch()
+    record['table_name'] = unique_name
+    record['t_name'] = t_name
+    record['t_category'] = t_cat
+    record['status'] = match.get('state')
+    record['is_doubles'] = '0'
+    record['is_quals'] = match.get('isQuals')
+    record['num_sets'] = match.get('numSets')
+    record['player1'] = '{} {}'.format(match.get('nAF'), match.get('nAL'))
+    record['player2'] = '{} {}'.format(match.get('nBF'), match.get('nBL'))
+    record['round'] = match.get('rnd')
+    record['winner'] = match.get('winner')
+    record['score'] = ''.join([match.get(s) for s in ['s{}{}'.format(num,side) for side in ['A','B'] for num in range(1,6)]])
+    record['first_server'] =  '' # TODO
+    record['start_ts'] = match.get('ts')
+    record['matchtime'] = match.get('mt')
+    record['retirement'] = '' # TODO
+    return record
 
 def getMatchRow(match, counter):
     set = getMatchSet(match)
